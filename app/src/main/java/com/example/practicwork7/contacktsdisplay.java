c2a5cbd509565af2ec190aff41ccbfd0ba3f2e4e
Егор <egorantonov1804@gmail.com>
2023-04-14 14:57:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/practicwork7/contacktsdisplay.java
@@ -0,0 +1,14 @@
+package com.example.practicwork7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class contacktsdisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts);
+    }
+}
